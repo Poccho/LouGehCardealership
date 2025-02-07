411d7b1779356af2897a8b2dbea0ad6eb5888e18
Roger Anthony D. Bairoy <108564918+Poccho@users.noreply.github.com>
2025-02-07 21:34:55 +0800

    login ui done

--- index.php
+++ index.php
@@ -1,1 +1,20 @@
-hello world
+<head>
+  <link rel="stylesheet" href="css/login.css">
+</head>
+
+<form class="login">
+  <div class="photo-placeholder">
+    <img src="documentation\assets\LouGehIconCropped.png" alt="LouGeh" id="LouGeh">
+  </div>
+  
+  <input type="text" placeholder="Username">
+  
+  <input type="password" placeholder="Password" id="password">
+  
+  <span id="togglePassword" class="toggle-password">Show password</span>
+  
+  <button type="submit">Login</button>
+</form>
+
+<script src="js\login.js"></script>
+
